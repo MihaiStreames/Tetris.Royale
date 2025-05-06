008051df952a8c646d866f3e19783f3f23e017b0
Taeo17 <tahasa32ni@gmail.com>
2025-05-06 16:06:53 +0200

    je vous promets les invits fct mtn

--- src/client/gui/MainMenus/FriendsListWidget.cpp
+++ src/client/gui/MainMenus/FriendsListWidget.cpp
@@ -278,7 +278,8 @@ void FriendsList::addFriendWidget(const QString &friendName) {
     connect(friendWidget, &FriendWidget::thirdButtonClicked, this, [this, friendName]() {
         std::string userID = session.getAccountIDFromUsername(friendName.toStdString());
         if (session.getOwnStatus() == ClientStatus::IN_LOBBY) {
-            if (session.getClientStatus(userID) == ClientStatus::OFFLINE) {
+            if (session.getClientStatus(friendName.toStdString()) == ClientStatus::OFFLINE)
+            {
                 QMessageBox::warning(this, "Invite Error", "This user is offline.");
                 return;
             }
